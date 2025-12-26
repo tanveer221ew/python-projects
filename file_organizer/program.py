bd919a7fd891963da611c8b6f6addb1f289d90c2
Tanveer Ahmed <tanveerchandio432@gmail.com>
2025-12-26 18:03:15 +0500

    Updated file organizer tool

--- file_organizer/program.py
+++ file_organizer/program.py
@@ -1,23 +1,43 @@
 import os
 import shutil
+import tkinter as tk
+from tkinter import filedialog, messagebox
 
-path_folder = r"C:\Users\Tanveer\Desktop\testfolder"
 extension_folders = {
     "pdf": "PDFs",
     "txt": "Text_Documents",
-    "jpg": "Images",  
+    "jpg": "Images",
     "png": "Images",
     "docx": "Word_Documents"
 }
 
-for file in os.listdir(path_folder):
-    extension = os.path.splitext(file)[1][1:].lower()  
-    folder_name = extension_folders.get(extension)     
+def organize_files():
+    path_folder = filedialog.askdirectory(title="Select Folder to Organize")
+    if not path_folder:
+        return
+    
+    for file in os.listdir(path_folder):
+        if os.path.isfile(os.path.join(path_folder, file)):
+            extension = os.path.splitext(file)[1][1:].lower()
+            folder_name = extension_folders.get(extension, "Other")  # Unknown files go to "Other"
 
-    if folder_name:
-        src_path = os.path.join(path_folder, file)
-        dest_folder = os.path.join(path_folder, folder_name)
-        dest_path = os.path.join(dest_folder, file)
+            src_path = os.path.join(path_folder, file)
+            dest_folder = os.path.join(path_folder, folder_name)
+            dest_path = os.path.join(dest_folder, file)
 
-        os.makedirs(dest_folder, exist_ok=True)
-        shutil.move(src_path, dest_path)
+            os.makedirs(dest_folder, exist_ok=True)
+            shutil.move(src_path, dest_path)
+    
+    messagebox.showinfo("Done!", f"Files in '{path_folder}' have been organized!")
+
+root = tk.Tk()
+root.title("File Organizer Tool")
+root.geometry("400x150")
+
+label = tk.Label(root, text="Organize files by type with one click!", font=("Arial", 12))
+label.pack(pady=10)
+
+btn = tk.Button(root, text="Select Folder to Organize", command=organize_files, width=25, height=2)
+btn.pack(pady=20)
+
+root.mainloop()
